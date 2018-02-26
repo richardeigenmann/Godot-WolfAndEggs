b888a7940a7727e71e70cfd299c64242170c010d
Richard Eigenmann <richard.eigenmann@gmail.com>
2018-02-27 00:35:19 +0100

    Better Egss, Physics and branches

--- Main.gd
+++ Main.gd
@@ -5,7 +5,7 @@ extends Node
 # var b = "textvar"
 # multiplyer for difficulty. Lower float = faster
 var difficulty = 1.1
-export (PackedScene) var Egg
+export (PackedScene) var NewEgg
 
 func _ready():
 	# Called every time the node is added to the scene.
@@ -21,16 +21,54 @@ func _ready():
 
 func startGame():
 	var waitTime = rand_range(0.5, 1.5)
-	print("startGame: ", waitTime)
 	$Chicken1Timer.set_wait_time( waitTime)
 	$Chicken1Timer.start()
+
+	waitTime = rand_range(0.5, 1.5)
+	$Chicken2Timer.set_wait_time( waitTime)
+	$Chicken2Timer.start()
+
+	waitTime = rand_range(0.5, 1.5)
+	$Chicken3Timer.set_wait_time( waitTime)
+	$Chicken3Timer.start()
 	
+	waitTime = rand_range(0.5, 1.5)
+	$Chicken4Timer.set_wait_time( waitTime)
+	$Chicken4Timer.start()
 
 
 func _on_Chicken1Timer_timeout():
-	print("Chicken1Timer_timeout")
-	var egg = Egg.instance()
+	var egg = NewEgg.instance()
 	add_child(egg)
-	var waitTime = rand_range(0.5, 1.5)
+	egg.position = Vector2(44,175)
+	var waitTime = rand_range(0.8, 2.5)
 	$Chicken1Timer.set_wait_time( waitTime)
 	$Chicken1Timer.start()
+
+func _on_Chicken2Timer_timeout():
+	var egg = NewEgg.instance()
+	add_child(egg)
+	egg.position = Vector2(44,315)
+	var waitTime = rand_range(0.8, 2.5)
+	$Chicken2Timer.set_wait_time( waitTime)
+	$Chicken2Timer.start()
+
+func _on_Chicken3Timer_timeout():
+	var egg = NewEgg.instance()
+	add_child(egg)
+	egg.position = Vector2(795,175)
+	var waitTime = rand_range(0.8, 2.5)
+	$Chicken3Timer.set_wait_time( waitTime)
+	$Chicken3Timer.start()
+
+func _on_Chicken4Timer_timeout():
+	var egg = NewEgg.instance()
+	add_child(egg)
+	egg.position = Vector2(795,315)
+	var waitTime = rand_range(0.8, 2.5)
+	$Chicken4Timer.set_wait_time( waitTime)
+	$Chicken4Timer.start()
+
+
+func _on_Area2D_body_entered( body ):
+	print("something entered me!")
--- NewEgg.gd
+++ NewEgg.gd
@@ -1,5 +1,6 @@
-extends Node
+extends RigidBody2D
 
+signal cracked
 
 # class member variables go here, for example:
 # var a = 2
@@ -8,9 +9,18 @@ extends Node
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
+	print("egg ready!")
 	pass
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
+
+
+func _on_Egg_body_entered( body ):
+	hide()
+	emit_signal("cracked")
+	print("cracked")
+	$CollisionShape2D.disabled = true
+
